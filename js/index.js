a9e457b5413a14bdd718520e799032a7ac5834b2
shawon-MG <work.shawomg@gmail.com>
2022-02-17 22:41:01 +0600

    brute force code for this webpage - added

--- js/index.js
+++ js/index.js
@@ -19,4 +19,43 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     else {
         balanceTextField.innerText = "You spended more then you earn!"
     }
+}); 
+// calculating saving amount after spending | event listner:
+document.getElementById('savings-btn').addEventListener('click', function(){
+    const savingsInputField = document.getElementById('savings-input-field').value;
+    const incomeField = document.getElementById('income-field').value;
+    const savingAmount = parseFloat(savingsInputField) * parseFloat(incomeField) / 100;
+ 
+    const foodPrice = document.getElementById('food-price').value;
+    const rentPrice = document.getElementById('rent-price').value;
+    const clothesPrice = document.getElementById('clothes-price').value;
+    const totalExpenses = parseFloat(foodPrice) + parseFloat(rentPrice) + parseFloat(clothesPrice);
+    
+    const totalInputField = parseFloat(incomeField);
+    const totalBalance = totalInputField - totalExpenses;
+
+    if (savingAmount < totalBalance){
+        const savingAmountText = document.getElementById('saving-amount');
+        savingAmountText.innerText = savingAmount;
+    }
+    else {
+        const savingAmountText = document.getElementById('saving-amount');
+        savingAmountText.innerText = "You spends more than you want to save!";
+    }
+
+    // const checkIncomeFieldText = document.getElementById('income-field').value;
+    // const checkIncomeField = parseFloat(checkIncomeFieldText);
+
+    const remainigBalance = totalBalance - savingAmount;
+    if (savingAmount < totalBalance) {
+        const remainigBalanceText = document.getElementById('remaining-balance');
+        remainigBalanceText.innerText = remainigBalance;
+    }
+    else{
+        const remainigBalanceText = document.getElementById('remaining-balance');
+        remainigBalanceText.innerText = "No Remaining Balance For Now!!!";
+    }
+
+    // const remainigBalanceText = document.getElementById('remaining-balance');
+    // remainigBalanceText.innerText = remainigBalance;
 });
